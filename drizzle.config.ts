ebf69244e6a6fd881c47c8fe1b2fa6f28b203ed1
Bismark Arroyo <bis.arroyo@gmail.com>
2025-08-23 15:13:45 -0600

    initial drizzle confiuration and schema validation

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/models/schema.ts",
+  out: "./src/db/migrations",
+  driver: "d1-http",
+  dbCredentials: {
+    url: process.env.TURSO_CONNECTION_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+  dialect: "sqlite",
+  verbose: true,
+  strict: true,
+});
