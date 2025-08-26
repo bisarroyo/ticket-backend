15bda122abfaf105898aa1c08863ec0578f7e2ab
Bismark Arroyo <bis.arroyo@gmail.com>
2025-08-25 18:26:15 -0600

    add logic for events controller

--- src/controllers/events.controller.ts
+++ src/controllers/events.controller.ts
@@ -1,6 +1,11 @@
 import type { Request, Response } from 'express'
 import { db } from '../database/db.ts'
-import { eventsTable, sectionsTable, venuesTable } from '../models/schema.js'
+import {
+    eventsTable,
+    sectionsTable,
+    venuesTable,
+    type Schema
+} from '../models/schema.js'
 import { eq } from 'drizzle-orm'
 import { CustomError } from '../lib/custom-error.js'
 
@@ -9,6 +14,8 @@ interface AuthRequest extends Request {
         userId: string
     }
 }
+
+type InsertEventsTable = Schema['InsertEventsTable']
 
 export const getAllEvents = async (req: Request, res: Response) => {
     try {
@@ -53,6 +60,8 @@ export const getEventById = async (req: Request, res: Response) => {
             .leftJoin(sectionsTable, eq(venuesTable.id, sectionsTable.venueId))
             .all()
 
+        console.log(event)
+
         if (!event.length) {
             throw new CustomError('Event not found', 404)
         }
@@ -64,106 +73,116 @@ export const getEventById = async (req: Request, res: Response) => {
     }
 }
 
-// export const createEvent = async (req: AuthRequest, res: Response) => {
-//   try {
-//     const userId = req.auth?.userId;
-//     if (!userId) {
-//       throw new CustomError("Unauthorized", 401);
-//     }
+export const createEvent = async (req: Request, res: Response) => {
+    try {
+        // todo validate user
+        // todo validate input
+        // todo concatenat venues and sections creation
+        const { name, eventImage, description } = req.body
+        const newEvent = await db.insert(eventsTable).values({
+            name,
+            date: new Date(),
+            startsAt: new Date(),
+            endsAt: new Date(),
+            eventImage,
+            description,
+            status: 'draft',
+            isActive: false,
+            isOnline: false,
+            capacity: 0
+        })
+        console.log(newEvent)
+        res.status(201).json(newEvent)
+    } catch (error) {
+        if (error instanceof CustomError) throw error
+        throw new CustomError('Error creating event', 500)
+    }
+}
 
-//     const eventData = {
-//       ...req.body,
-//       userId,
-//       date: new Date(req.body.date),
-//       startsAt: new Date(req.body.startsAt),
-//       endsAt: new Date(req.body.endsAt),
-//     };
+export const updateEvent = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const { name, eventImage, description } = req.body
+        if (!id) throw new CustomError('Event ID is required', 400)
 
-//     const newEvent = await db.insert(eventsTable).values(eventData);
-//     return res.status(201).json(newEvent);
-//   } catch (error) {
-//     if (error instanceof CustomError) throw error;
-//     throw new CustomError("Error creating event", 500);
-//   }
-// };
+        const userId = '123' // temp fallback userId for testing
+        if (!userId) {
+            throw new CustomError('Unauthorized', 401)
+        }
 
-// export const updateEvent = async (req: AuthRequest, res: Response) => {
-//   try {
-//     const { id } = req.params;
-//     if (!id) throw new CustomError("Event ID is required", 400);
+        const event = await db
+            .select()
+            .from(eventsTable)
+            .where(eq(eventsTable.id, parseInt(id)))
 
-//     const userId = req.auth?.userId;
-//     if (!userId) {
-//       throw new CustomError("Unauthorized", 401);
-//     }
+        if (!event.length) {
+            throw new CustomError('Event not found', 404)
+        }
 
-//     const event = await db
-//       .select()
-//       .from(eventsTable)
-//       .where(eq(eventsTable.id, parseInt(id)));
+        if (event[0]?.userId !== userId) {
+            throw new CustomError(
+                'Unauthorized - You can only update your own events',
+                403
+            )
+        }
 
-//     if (!event.length) {
-//       throw new CustomError("Event not found", 404);
-//     }
+        const updateData = {
+            ...req.body,
+            updatedAt: new Date().toISOString(),
+            date: new Date(req.body.date),
+            startsAt: new Date(req.body.startsAt),
+            endsAt: new Date(req.body.endsAt),
+            eventImage,
+            description,
+            status: 'draft',
+            isActive: false,
+            isOnline: false,
+            capacity: 0
+        }
 
-//     if (event[0].userId !== userId) {
-//       throw new CustomError(
-//         "Unauthorized - You can only update your own events",
-//         403
-//       );
-//     }
+        await db
+            .update(eventsTable)
+            .set(updateData)
+            .where(eq(eventsTable.id, parseInt(id)))
 
-//     const updateData = {
-//       ...req.body,
-//       updatedAt: new Date().toISOString(),
-//       date: new Date(req.body.date),
-//       startsAt: new Date(req.body.startsAt),
-//       endsAt: new Date(req.body.endsAt),
-//     };
+        return res.status(200).json({ message: 'Event updated successfully' })
+    } catch (error) {
+        if (error instanceof CustomError) throw error
+        throw new CustomError('Error updating event', 500)
+    }
+}
 
-//     await db
-//       .update(eventsTable)
-//       .set(updateData)
-//       .where(eq(eventsTable.id, parseInt(id)));
+export const deleteEvent = async (req: AuthRequest, res: Response) => {
+    try {
+        const { id } = req.params
+        if (!id) throw new CustomError('Event ID is required', 400)
 
-//     return res.status(200).json({ message: "Event updated successfully" });
-//   } catch (error) {
-//     if (error instanceof CustomError) throw error;
-//     throw new CustomError("Error updating event", 500);
-//   }
-// };
+        const userId = req.auth?.userId
+        if (!userId) {
+            throw new CustomError('Unauthorized', 401)
+        }
 
-// export const deleteEvent = async (req: AuthRequest, res: Response) => {
-//   try {
-//     const { id } = req.params;
-//     if (!id) throw new CustomError("Event ID is required", 400);
+        const event = await db
+            .select()
+            .from(eventsTable)
+            .where(eq(eventsTable.id, parseInt(id)))
 
-//     const userId = req.auth?.userId;
-//     if (!userId) {
-//       throw new CustomError("Unauthorized", 401);
-//     }
+        if (!event.length) {
+            throw new CustomError('Event not found', 404)
+        }
 
-//     const event = await db
-//       .select()
-//       .from(eventsTable)
-//       .where(eq(eventsTable.id, parseInt(id)));
+        if (event[0]?.userId !== userId) {
+            throw new CustomError(
+                'Unauthorized - You can only delete your own events',
+                403
+            )
+        }
 
-//     if (!event.length) {
-//       throw new CustomError("Event not found", 404);
-//     }
+        await db.delete(eventsTable).where(eq(eventsTable.id, parseInt(id)))
 
-//     if (event[0].userId !== userId) {
-//       throw new CustomError(
-//         "Unauthorized - You can only delete your own events",
-//         403
-//       );
-//     }
-
-//     await db.delete(eventsTable).where(eq(eventsTable.id, parseInt(id)));
-
-//     return res.status(200).json({ message: "Event deleted successfully" });
-//   } catch (error) {
-//     if (error instanceof CustomError) throw error;
-//     throw new CustomError("Error deleting event", 500);
-//   }
-// };
+        return res.status(200).json({ message: 'Event deleted successfully' })
+    } catch (error) {
+        if (error instanceof CustomError) throw error
+        throw new CustomError('Error deleting event', 500)
+    }
+}
