befec26846acc6847b1c90d4e9a799743b294ec7
Bismark Arroyo <bis.arroyo@gmail.com>
2025-08-25 18:25:42 -0600

    config drizzle scripts and config for node with turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,13 @@
-import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
+import 'dotenv/config'
 
-export default defineConfig({
-  schema: "./src/models/schema.ts",
-  out: "./src/db/migrations",
-  driver: "d1-http",
-  dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  },
-  dialect: "sqlite",
-  verbose: true,
-  strict: true,
-});
+import type { Config } from 'drizzle-kit'
+
+export default {
+    schema: './src/models/schema.ts',
+    out: './src/bd/migrations',
+    dialect: 'turso',
+    dbCredentials: {
+        url: process.env.TURSO_DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN ?? ''
+    }
+} satisfies Config
