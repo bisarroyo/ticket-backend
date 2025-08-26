59733e86e553faead38c9b3f70b41daf89a80ced
Bismark Arroyo <bis.arroyo@gmail.com>
2025-08-25 18:25:13 -0600

    added all routes

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,57 @@
-import "dotenv/config";
+import 'dotenv/config'
 
-import express, {
-  type Request,
-  type Response,
-  urlencoded,
-  json,
-} from "express";
-import { clerkMiddleware } from "@clerk/express";
-import cors from "cors";
+import express, { type Request, type Response, urlencoded, json } from 'express'
+// import { clerkMiddleware } from "@clerk/express";
+import { clerkClient, requireAuth, getAuth } from '@clerk/express'
+import cors from 'cors'
 
 // custom middlewares
-import { notFound } from "./middleware/not-found.js";
-import { error } from "./middleware/error.js";
+import { notFound } from './middleware/not-found.js'
+import { error } from './middleware/error.js'
 
-import { logRequestMethod, logHostname } from "./middleware/logMiddleware.js";
+import { logRequestMethod, logHostname } from './middleware/logMiddleware.js'
 
-import eventsRoutes from "./routes/events.route.js";
-// import mailsRoutes from "@/routes/mails.route.js";
-// import paymentsRoutes from "@/routes/payments.route.js";
-// import ticketsRoutes from "@/routes/tickets.route.js";
+import eventsRoutes from './routes/events.route.js'
+import mailsRoutes from '@/routes/mails.route.js'
+import paymentsRoutes from '@/routes/payments.route.js'
+import ticketsRoutes from '@/routes/tickets.route.js'
 
-const app = express();
-const port = process.env.PORT;
+const app = express()
+const port = process.env.PORT
 
-app.use(urlencoded({ extended: true }));
-app.use(json());
+app.use(urlencoded({ extended: true }))
+app.use(json())
 
 // handle cors
-app.use(cors());
+app.use(cors())
 
-app.use(logRequestMethod);
-app.use(logHostname);
+app.use(logRequestMethod)
+app.use(logHostname)
 
 // handle auth
-app.use(clerkMiddleware());
+// app.use(clerkMiddleware());
 
-app.get("/", (request: Request, response: Response) => {
-  response.send("Hello World!!!!");
-});
+app.get('/', (request: Request, response: Response) => {
+    response.send('Hello World')
+})
 
-app.use("/api/v1/events", eventsRoutes);
-// app.use("/api/v1/mail", mailsRoutes);
-// app.use("/api/v1/payments", paymentsRoutes);
-// app.use("/api/v1/payments", ticketsRoutes);
+app.use('/api/v1/events', eventsRoutes)
+app.use('/api/v1/mail', mailsRoutes)
+app.use('/api/v1/payments', paymentsRoutes)
+app.use('/api/v1/payments', ticketsRoutes)
 
-app.use(notFound);
-app.use(error);
+app.get('/protected', requireAuth(), async (req: Request, res: Response) => {
+    const { userId } = getAuth(req)
+    if (!userId) {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+    const user = await clerkClient.users.getUser(userId)
+    res.status(200).json({ message: 'Protected route', user })
+})
+
+app.use(notFound)
+app.use(error)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
