3f7c681f25597caf4cff40a1d63958a53a8a14f7
Bismark Arroyo <bis.arroyo@gmail.com>
2025-08-26 22:25:02 -0600

    remove no necesary routes

--- src/routes/mails.route.ts
+++ src/routes/mails.route.ts
@@ -5,11 +5,5 @@ import { sendEmail } from '../controllers/mails.controller.ts'
 const router = Router()
 
 router.post('/send', sendEmail)
-// router.get('/id', getClientById)
-// router.get('/email', getClientByEmail)
-// router.get('/name', getClientByName)
-// router.post('/create', insertClient)
-// router.put('/update', updateClient)
-// router.delete('/remove', removeClient)
 
 export default router
