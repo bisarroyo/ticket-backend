185421a7b7e89035f38f79b8af008daf78a20243
Bismark Arroyo <bis.arroyo@gmail.com>
2025-09-03 16:23:37 -0600

    add log path  middleware

--- src/middleware/logMiddleware.ts
+++ src/middleware/logMiddleware.ts
@@ -1,18 +1,22 @@
-import { type Request, type Response, type NextFunction } from "express";
+import { type Request, type Response, type NextFunction } from 'express'
 
 export const logRequestMethod = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  console.log(req.method);
-  next();
-};
+    console.log(req.method)
+    next()
+}
 export const logHostname = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  console.log(req.hostname);
-  next();
-};
+    console.log(req.hostname)
+    next()
+}
+export const logPath = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.path)
+    next()
+}
